4c3c867494844f28b682e05a96091c01f6a62fd7
Ounsesa <carlos.rocal01@gmail.com>
2024-02-19 18:39:44 +0100

    Init

---
+++ Fisica/Source/Fisica/Fisica.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Fisica.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Fisica, "Fisica" );
+
+DEFINE_LOG_CATEGORY(LogFisica)
+ 
